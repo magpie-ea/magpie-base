514d1f66df251d01552c13be6afc570c90faa7a0
Marcel Klehr <mklehr@gmx.net>
2020-08-31 12:13:06 +0200

    Add styleguidist config

---
+++ styleguide.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  // set your styleguidist configuration here
+  title: 'Magpie',
+  require: [path.join(__dirname, 'styleguide/global.requires.js')],
+  // components: 'src/components/**/[A-Z]*.vue',
+  // defaultExample: true,
+  sections: [
+    {
+      name: 'Base Components',
+      components: 'src/components/*.vue'
+    },
+    {
+      name: 'Inputs',
+      components: 'src/components/inputs/*.vue'
+    },
+    {
+      name: 'Screens',
+      components: 'src/components/screens/*.vue'
+    }
+  ],
+  // webpackConfig: {
+  //   // custom config goes here
+  // },
+  exampleMode: 'expand'
+};
--- styleguide/global.requires.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue';
-import VueMagpie from '../src/plugin';
-
-Vue.use(VueMagpie);
