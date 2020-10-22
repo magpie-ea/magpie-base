6c5d5ae5a6ac3b5dbc70d89e193c13476cd7f7e8
Marcel Klehr <mklehr@gmx.net>
2020-10-22 14:20:04 +0200

    Implement Timer helper

---
+++ src/components/helpers/Timer.vue
@@ -0,0 +1,50 @@
+<docs>
+```vue
+<Experiment>
+  <template #screens>
+    <Screen>
+      <template #0="{nextSlide}">
+        Slide 1<br />
+        <Timer v-model="timer"></Timer>
+        <button @click="nextSlide">Next slide</button>
+      </template>
+      <template #1="{nextSlide}">
+        Slide 2<br />
+        Time until click: {{timer()/1000}} seconds
+      </template>
+    </Screen>
+  </template>
+</Experiment>
+```
+</docs>
+<template><span /></template>
+<script>
+export default {
+  name: 'Timer',
+  mounted() {
+    this.start();
+  },
+  updated() {
+    this.start();
+  },
+  activated() {
+    this.start();
+  },
+  data() {
+    return {
+      startTime: null
+    };
+  },
+  methods: {
+    time() {
+      return Date.now() - this.startTime;
+    },
+    start() {
+      this.startTime = Date.now();
+      this.$emit('input', this.time);
+    }
+  }
+};
+</script>
+
+<style scoped></style>
--- styleguidist/global.requires.js
+++ styleguidist/global.requires.js
@@ -2,3 +2,9 @@ import Vue from 'vue';
 import MagpieVue from '../src/index';
 
 Vue.use(MagpieVue);
+Vue.mixin({
+  data() {
+    // For the timer docs
+    return { timer: null };
+  }
+});
