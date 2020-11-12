570c4413ba894199df221244723c499044ef007e
Marcel Klehr <mklehr@gmx.net>
2020-11-12 12:29:26 +0100

    Example app: Add more comments

--- lib/templates/src/App.vue
+++ lib/templates/src/App.vue
@@ -1,4 +1,7 @@
 <template>
+  <!--
+  Set your experiment id here and
+  Define your data sources with the trials attribute -->
   <Experiment
     id="test1234"
     :trials="{
@@ -9,10 +12,12 @@
       sliderRating
     }"
   >
+    <!-- The contents of the #title template slot will be displayed in the upper left corner of the experiment -->
     <template #title>
       <div>The experiment</div>
     </template>
 
+    <!-- The contents of the #screens template slot define your experiment -->
     <template #screens>
       <Screen :title="'Welcome'">
         This is a sample introduction screen.
@@ -23,6 +28,7 @@
         <br />
         <br />
         This mock up experiment is a showcase of the functionality of magpie.
+        <!-- The $exp field gives you access to magpie-specific functionality -->
         <button @click="$exp.nextScreen">Begin the experiment</button>
       </Screen>
 
@@ -36,9 +42,13 @@
       </Screen>
 
       <!-- Practice trials -->
+      <!-- Here we create screens in a loop for every entry in forced_choice -->
       <template v-for="i in forced_choice_length">
         <Screen :key="'forcedchoice-' + i">
           <template #0>
+            <!-- We automatically retrieve trial data from our data sources using $exp.trial.<data-source>
+            Once the participant has made a choice, the change:answer event fires and we save the answer and progress to the next screeen.
+            -->
             <img :src="$exp.trial.forced_choice.picture" alt="" />
             <ForcedChoiceInput
               :question="$exp.trial.forced_choice.question"
@@ -183,9 +193,11 @@
 </template>
 
 <script>
+// Load data from csv files as javascript arrays with objects
 import forced_choice from '../trials/forced_choice.csv';
 import multi_dropdown from '../trials/multi_dropdown.csv';
 import sentenceChoice from '../trials/sentence_choice.csv';
+import _ from 'lodash';
 
 export default {
   name: 'App',
@@ -253,7 +265,7 @@ export default {
       multi_dropdown_length: multi_dropdown.length,
       sentenceChoice,
       sentenceChoice_length: sentenceChoice.length,
-      imageSelection: imageSelection,
+      imageSelection: _.shuffle(imageSelection),
       imageSelection_length: imageSelection.length,
       sliderRating,
       sliderRating_length: sliderRating.length,
