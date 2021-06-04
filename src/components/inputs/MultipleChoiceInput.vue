21e6d7b1446217df64e8ef2eb518511622b88051
Marcel Klehr <mklehr@gmx.net>
2021-06-04 13:51:11 +0200

    Implement {single, matrix} multiple choice

---
+++ src/components/inputs/MultipleChoiceInput.vue
@@ -0,0 +1,126 @@
+<docs>
+
+```vue
+<Experiment>
+  <template #screens>
+
+    <Screen>
+
+      <template #0="{measurements}">
+        <p>How was your breakfast?</p>
+        <MultipleChoiceInput
+            :response.sync="measurements.breakfast"
+            :options="['Not enjoyable', 'Rather not enjoyable', 'OK', 'Rather enjoyable', 'Really enjoyable']" />
+        <button @click="$magpie.addTrialData(measurements); $magpie.nextScreen();">Submit</button>
+      </template>
+
+    </Screen>
+
+  </template>
+</Experiment>
+```
+
+
+```vue
+<Experiment>
+  <template #screens>
+
+    <Screen>
+
+      <template #0="{measurements}">
+        <p>How was your breakfast?</p>
+        <MultipleChoiceInput
+            :response.sync="measurements.breakfast"
+            orientation="horizontal"
+            :options="['Not enjoyable', 'Rather not enjoyable', 'OK', 'Rather enjoyable', 'Really enjoyable']" />
+        <button @click="$magpie.addTrialData(measurements); $magpie.nextScreen();">Submit</button>
+      </template>
+
+    </Screen>
+
+  </template>
+</Experiment>
+```
+
+</docs>
+
+<template>
+  <div :class="['multiple-choice', orientation]">
+    <form>
+      <div class="options">
+        <label v-for="(option, i) in options" :key="i"
+          ><input
+            :value="option"
+            type="radio"
+            name="options"
+            @input="$emit('update:response', option)"
+          />
+          <template v-if="labels">{{ option }}</template></label
+        >
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+/**
+ * Have the participant choose between multiple options.
+ */
+export default {
+  name: 'MultipleChoiceInput',
+  props: {
+    /**
+     * The possible options to choose from
+     */
+    options: {
+      type: Array,
+      required: true
+    },
+    /**
+     * Whether to display 'vertical' or 'horizontal'
+     */
+    orientation: {
+      type: String,
+      default: 'vertical'
+    },
+    /**
+     * Whether to display labels
+     */
+    labels: {
+      type: Boolean,
+      default: true
+    }
+  },
+  methods: {
+    onOptionClick(option) {
+      /**
+       * Change event with the chosen option. Useful in combination with `response.sync`
+       */
+      this.$emit('update:response', option);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.options label,
+.options label * {
+  cursor: pointer;
+}
+.multiple-choice.vertical {
+  text-align: left;
+  width: 500px;
+  margin: 0 auto;
+}
+.multiple-choice.vertical label {
+  display: block;
+}
+.multiple-choice.horizontal label {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+.option:hover {
+  background-color: #324d93;
+}
+</style>
---
+++ src/components/inputs/MultipleChoiceMatrixInput.vue
@@ -0,0 +1,183 @@
+<docs>
+
+```vue
+<Experiment>
+  <template #screens>
+
+    <Screen>
+
+      <template #0="{measurements}">
+        <MultipleChoiceMatrixInput
+            :options="['Not enjoyable', 'Rather not enjoyable', 'OK', 'Rather enjoyable', 'Really enjoyable']"
+            :questions="[
+              'How is the weather today?',
+              'How was your breakfast today?',
+              'How do you like your job?',
+              'What is your opinion of television?',
+              'What is your opinion of folk music?'
+            ]"
+            :responses.sync="measurements.responses"
+        />
+        <button @click="$magpie.addTrialData(measurements); $magpie.nextScreen();">Submit</button>
+      </template>
+
+    </Screen>
+
+    <DebugResultsScreen />
+
+  </template>
+</Experiment>
+```
+
+You can also randomize the question order. The response data will still be in the order you supplied the questions in.
+
+```vue
+<Experiment>
+  <template #screens>
+
+    <Screen>
+
+      <template #0="{measurements}">
+        <MultipleChoiceMatrixInput
+            :options="['Not enjoyable', 'Rather not enjoyable', 'OK', 'Rather enjoyable', 'Really enjoyable']"
+            :questions="[
+              'How is the weather today?',
+              'How was your breakfast today?',
+              'How do you like your job?',
+              'What is your opinion of television?',
+              'What is your opinion of folk music?'
+            ]"
+            :randomize="true"
+            :responses.sync="measurements.responses"
+        />
+        <button @click="$magpie.addTrialData(measurements); $magpie.nextScreen();">Submit</button>
+      </template>
+
+    </Screen>
+
+    <DebugResultsScreen />
+
+  </template>
+</Experiment>
+```
+
+
+</docs>
+
+<template>
+  <div class="matrix-choice">
+    <div class="row header">
+      <p></p>
+      <div>
+        <label v-for="option in options" :key="option" v-text="option" />
+      </div>
+    </div>
+    <template v-if="randomize">
+      <template v-for="[question, i] in randomizedQuestions">
+        <div :key="i" class="row">
+          <p>{{ question }}</p>
+          <MultipleChoiceInput
+            :labels="false"
+            :options="options"
+            orientation="horizontal"
+            @update:response="onOptionClick(i, $event)"
+          />
+        </div>
+      </template>
+    </template>
+    <template v-else>
+      <template v-for="(question, i) in questions">
+        <div :key="i" class="row">
+          <p>{{ question }}</p>
+          <MultipleChoiceInput
+            :labels="false"
+            :options="options"
+            orientation="horizontal"
+            @update:response="onOptionClick(i, $event)"
+          />
+        </div>
+      </template>
+    </template>
+    <div class="row header">
+      <p></p>
+      <div>
+        <label v-for="option in options" :key="option" v-text="option" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash';
+import MultipleChoiceInput from '@/components/inputs/MultipleChoiceInput';
+/**
+ * Have the participant choose between multiple options.
+ */
+export default {
+  name: 'MultipleChoiceMatrixInput',
+  components: { MultipleChoiceInput },
+  props: {
+    /**
+     * The questions to ask
+     */
+    questions: {
+      type: Array,
+      required: true
+    },
+    /**
+     * The possible answers to choose from
+     */
+    options: {
+      type: Array,
+      required: true
+    },
+    /**
+     * Whether to shuffle questions
+     */
+    randomize: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      responses: [],
+      randomizedQuestions: _.shuffle(
+        this.questions.map((question, i) => [question, i])
+      )
+    };
+  },
+  methods: {
+    onOptionClick(i, response) {
+      this.responses[i] = response;
+      /**
+       * Change event with the chosen options.
+       */
+      this.$emit('update:responses', this.responses);
+    }
+  }
+};
+</script>
+
+<style>
+.matrix-choice {
+  text-align: left;
+  width: 100%;
+}
+.matrix-choice .row {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.matrix-choice .row > :first-child {
+  width: 50%;
+  text-align: right;
+}
+.matrix-choice .row label {
+  width: 50px;
+  display: inline-block;
+  font-size: 13px;
+  margin: 0 10px;
+  text-align: center;
+}
+</style>
