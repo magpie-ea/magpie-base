9bd50d52106a08f747f6de19f539bde5262370d0
Marcel Klehr <mklehr@gmx.net>
2021-07-27 20:17:15 +0200

    Fix magpie onload mock

--- src/plugin.js
+++ src/plugin.js
@@ -2,9 +2,10 @@ import * as components from './components';
 
 export default function (Vue, config) {
   // universal mock to cover up that fact that magpie object is not available from start
-  const obj = () => magpie;
-  obj.valueOf = () => '';
-  const magpie = new Proxy(obj, { get: () => magpie });
+  // will always return itself when properties are accessed except for valueOf which returns () => '' to avoid errors
+  const magpie = new Proxy(() => magpie, {
+    get: (_, prop) => (prop !== 'valueOf' ? magpie : () => '')
+  });
 
   // auto-import all components
   Vue.mixin({
