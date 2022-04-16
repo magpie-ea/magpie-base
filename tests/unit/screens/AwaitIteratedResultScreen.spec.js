e402f6857d71baa45107d9a49b7920161f74137a
Marcel Klehr <mklehr@gmx.net>
2022-04-16 17:44:04 +0200

    Add integration test: Test race condition in AwaitIteratedResultScreen

--- tests/unit/screens/AwaitIteratedResultScreen.spec.js
+++ tests/unit/screens/AwaitIteratedResultScreen.spec.js
@@ -4,6 +4,8 @@ import Experiment from "@/components/Experiment";
 import {states } from '../../../src/Socket'
 import {createDynamicExperiment, createInteractiveExperiment, magpieConfig} from "../../helpers";
 
+jest.setTimeout(10000);
+
 test = process.env.MAGPIE_INTEGRATION_TESTS === 'true'? test : test.skip
 
 test('AwaitIteratedResultScreen with two clients', async () => {
@@ -50,6 +52,8 @@ test('AwaitIteratedResultScreen with two clients', async () => {
         await new Promise(resolve => setTimeout(resolve, 200))
     }
 
+    expect(experiment1.vm.$magpie.socket.generation).not.toBe(experiment2.vm.$magpie.socket.generation)
+
     expect(experiment1.text()).toBe('Connected')
     expect(experiment2.text()).toBe('Connecting')
 
@@ -71,4 +75,68 @@ test('AwaitIteratedResultScreen with two clients', async () => {
     expect(experiment2.text()).toBe('Connected')
     expect(experiment2.vm.$magpie.socket.lastIterationResults).not.toHaveLength(0)
     expect(experiment2.vm.$magpie.socket.lastIterationResults[0].response).toBe('foobar')
+})
+
+test('AwaitIteratedResultScreen with two clients and race condition', async () => {
+    const experimentId = await createDynamicExperiment(1, 1, 2)
+
+    const experiment1 = mount(Experiment, {
+        slots: {
+            default: [
+                '<AwaitIteratedResultScreen>Connecting</AwaitIteratedResultScreen>' +
+                '<Screen>Connected</Screen>',
+                '<SubmitResultsScreen />',
+            ]
+        },
+        magpie: {
+            ...magpieConfig,
+            experimentId,
+        }
+    })
+
+    expect(experiment1.text()).toBe('Connecting')
+
+    const experiment2 = mount(Experiment, {
+        slots: {
+            default: [
+                '<AwaitIteratedResultScreen>Connecting</AwaitIteratedResultScreen>' +
+                '<Screen>Connected</Screen>',
+                '<SubmitResultsScreen />',
+            ]
+        },
+        magpie: {
+            ...magpieConfig,
+            experimentId,
+        }
+    })
+
+    expect(experiment2.text()).toBe('Connecting')
+
+    while (experiment1.vm.$magpie.socket.iteratedState !== states.READY && experiment2.vm.$magpie.socket.state !== states.READY) {
+        await new Promise(resolve => setTimeout(resolve, 200))
+    }
+
+    let firstExp, secondExp
+
+    if (experiment1.vm.$magpie.socket.iteratedState === states.READY) {
+        firstExp = experiment1
+        secondExp = experiment2
+    }else{
+        firstExp = experiment2
+        secondExp = experiment1
+    }
+
+    firstExp.vm.$magpie.saveAndNextScreen()
+
+    while (secondExp.vm.$magpie.socket.iteratedState !== states.READY) {
+        await new Promise(resolve => setTimeout(resolve, 200))
+    }
+
+    expect(experiment1.vm.$magpie.socket.generation).not.toBe(experiment2.vm.$magpie.socket.generation)
+
+    await experiment1.vm.$nextTick()
+    await experiment2.vm.$nextTick()
+
+    expect(firstExp.text()).toContain('All done.')
+    expect(secondExp.text()).toBe('Connected')
 })
